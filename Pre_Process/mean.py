9273bc34afe567b1e80c8800989f4760e0e4c45d
Adrian Fernandez <adriandfl99@gmail.com>
2024-01-20 02:37:57 -0500

    Main app modified.

--- Pre_Process/mean.py
+++ Pre_Process/mean.py
@@ -2,8 +2,19 @@
 from pandas import DataFrame
 import pandas as pd
 def mean_df(data: DataFrame):
+    meandata = []
     dataMean = data.groupby('movie_title').agg(avg_rating = ('rating', 'mean'),
                                                 number_of_ratings = ('rating', 'count')).reset_index()
     avg_ratings100 = dataMean[dataMean['number_of_ratings']>100]
     
-    return avg_ratings100
+    sortedData = sorted(avg_ratings100.values.tolist(), key=lambda x: x[1],reverse=True)
+    
+    for movie in sortedData:
+        dict = {
+            'movie': movie[0],
+            'rating': movie[1]
+        }
+        meandata.append(dict)
+    
+    
+    return meandata
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import json
 from fastapi import FastAPI, BackgroundTasks, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 import uvicorn
+from Pre_Process.mean import mean_df
+
+from Pre_Process.processingDataset import averageData, process_Data
 
 app = FastAPI()
 
@@ -16,5 +21,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/recommend")
+def recommendMovies():
+    with open('./data.json',mode = 'r') as file:
+        data = json.load(file)  
+    with open('./item.json',mode = 'r') as file:
+        item = json.load(file)
+    dfMerge_Data_Item = process_Data(data,item)
+    meanData = mean_df(dfMerge_Data_Item)
+    
+    return {'Recommend_Movies': meanData}
+    
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    
+    
